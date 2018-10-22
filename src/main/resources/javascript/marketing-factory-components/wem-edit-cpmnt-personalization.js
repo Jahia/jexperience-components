91c8ea48c5908ef04d7c55ca610d12e8110e0f24
dgaillard <dgaillard@jahia.com>
2018-10-22 19:09:01 +0200

    DMF-3087 implement new personalization editor

--- src/main/resources/javascript/marketing-factory-components/wem-edit-cpmnt-personalization.js
+++ src/main/resources/javascript/marketing-factory-components/wem-edit-cpmnt-personalization.js
@@ -13,25 +13,23 @@ function customEditComponentPersoEngineGetAngularScope() {
 }
 
 function componentPersonalizationConditionsInit(data) {
-    console.log(data);
     if (data.isNewNode) {
-        console.log("IF");
-        return $.parseHTML("<h1>Please click on save button to create your carousel then add carousel item and you will be able to access this panel!</h1>");
+        return $.parseHTML("<h1>Please click on save button to create your personalized component then add variants and you will be able to access this panel!</h1>");
     }
     return $.parseHTML("<iframe id=\"conditionFrame\" width=\"100%\" height=\"100%\" frameborder='0' src=\"" + jahiaGWTParameters.contextPath + jahiaGWTParameters.servletPath + "/editframe/default/" + jahiaGWTParameters.lang + data.getPath() + ".wem-edit-engine-cpmnt-perso.html\"/>")[0];
 }
 
 function componentPersonalizationConditionsDoSave(node) {
     var angularScope = customEditComponentPersoEngineGetAngularScope();
-    if (angularScope) {
-        angularScope.save(node);
+    if (angularScope && angularScope.$parent && angularScope.$parent.wemPersonalizationEditorCtrl) {
+        angularScope.$parent.wemPersonalizationEditorCtrl.gwtSave(node);
     }
 }
 
 function componentPersonalizationConditionsDoValidate(validation) {
     var angularScope = customEditComponentPersoEngineGetAngularScope();
-    if (angularScope) {
-        angularScope.validate(validation);
+    if (angularScope && angularScope.$parent && angularScope.$parent.wemPersonalizationEditorCtrl) {
+        angularScope.$parent.wemPersonalizationEditorCtrl.gwtValidation(validation);
     }
 }
 
